6b2c053914fb76b3226ec01e801da9363369bf4e
CloudMojos <joshuel.simbulan@gmail.com>
2023-02-20 21:45:49 +0800

    attempt 4kyu/so-many-permutations

--- 4kyu/so-many-permutations/solution.js
+++ 4kyu/so-many-permutations/solution.js
@@ -1,4 +1,10 @@
 function permutations(string) {
-    
-	return [];
+	const strArr = string.split('')
+	
+	for (let i = 0; i < strArr.length; i++) {
+		console.log(strArr[i])
+	}
 }
+
+// console.log(permutations('abc'))
+permutations('abc')
