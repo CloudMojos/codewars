3cccebefdb203d0d420a1af865c839ce9b421562
CloudMojos <59500348+CloudMojos@users.noreply.github.com>
2023-10-02 20:21:53 +0800

    Delete 8kyu/are_you_playing_banjo? directory

--- 8kyu/are_you_playing_banjo?/solution.js
+++
@@ -1,9 +0,0 @@
-function areYouPlayingBanjo(name) {
-    // Implement me
-    if (name[0] === 'R' || name[0] === 'r') {
-      name += " plays banjo"
-    } else {
-      name += " does not play banjo"
-    }
-    return name;
-  }
