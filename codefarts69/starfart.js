c470b8eeea4ef00f037fa1a4c349022e9d8f69a3
starfart69 <joshuel.simbulan@gmail.com>
2024-05-18 10:11:13 +0800

    pretty much done. kulang na lang ng command line args

--- codefarts69/starfart.js
+++ codefarts69/starfart.js
@@ -39,14 +39,67 @@ async function extractKyuLevel(page) {
 }
 
 
+async function extractTitle(page) {
+    try {
+        // Extract the description element's content
+        const title = await page.$eval('.game-title h4', el => el.textContent);
+        return title;
+    } catch (error) {
+        console.error('Error extracting description:', error);
+    }
+}
+
+async function saveDescriptionToFile(description, folderPath, filename) {
+    try {
+        const filePath = path.join(folderPath, filename);
+        fs.writeFileSync(filePath, description, 'utf8');
+        console.log(`Description saved to ${filePath}`);
+    } catch (error) {
+        console.error('Error writing to file:', error);
+    }
+}
+
+function copyPasteFile(source, dest) {
+    fs.copyFile(source, dest, (err) => {
+        if (err) {
+            console.error("Couldn't copy file: ", err);
+        } else {
+            console.log("Copied!")
+        }
+    })    
+}
+
 async function main() {
     // Call the function with the URL
-    const { browser, page } = await codeWars('https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript');
+    // const { browser, page } = await codeWars('https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript');
+    const { browser, page } = await codeWars('https://www.codewars.com/kata/526d84b98f428f14a60008da/train/javascript');
     
     const description = await extractDescription(page);
-    const kyu = await extractKyuLevel(page);
-    console.log(kyu);
-    
+    let kyu = await extractKyuLevel(page);
+    // One way of trimming the space between the number and kyu xD
+    kyu = kyu[0].concat('kyu');
+    let title = await extractTitle(page);
+    // But I can't cheat it the same way with the title T.T
+    title = title.replace(/\s/g, "_").toLowerCase();
+    console.log(description)
+
+    const folderPath = path.join(__dirname, '..', kyu, title);
+
+    fs.mkdir(folderPath, { recursive: true }, (err) => {
+        if (err) {
+            return console.error(`Error creating directory: ${err.message}`);
+        }
+        console.log(`Directory ${folderPath} created successfully!`);
+    });
+
+    // Save the description to a text file
+    await saveDescriptionToFile(description, folderPath, `${title}.desc`);
+
+    const tempJS = path.join(__dirname, 'temp.js')
+    const destJS = path.join(folderPath, `${title}.js`)
+    console.log(tempJS)
+    copyPasteFile(tempJS, destJS);
+
     // Close the browser
     await browser.close();
 }
---
+++ codefarts69/temp.js
@@ -0,0 +1,7 @@
+function solution(arg) {
+    
+}
+
+const arg = null
+const x = solution(arg)
+console.log(x)
